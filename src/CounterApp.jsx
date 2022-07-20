ee49c2b1fafe5512b2a207053c1dcbe7c1f1c1b3
PacoNavarrete <francisco1navarrete@gmail.com>
2022-07-20 16:34:46 -0700

    2 files modified, 2 added, 5 deleted

---
+++ src/CounterApp.jsx
@@ -0,0 +1,12 @@
+// import ProTypes from 'prop-types';
+
+export const CounterApp = ({ value }) => {
+  return (
+    <>
+      <h1> Counter App</h1>
+      <h2> {value} </h2>
+    </>
+  );
+};
+
+// CounterApp.proTypes = {};
